6f54e2005151a0361f6889e4e93b012aafc56930
RagehAz <rageh.az@gmail.com>
2024-01-08 23:46:23 +0200

    5.5.3 : tamam

--- lib/helpers/classes/strings/pathing.dart
+++ lib/helpers/classes/strings/pathing.dart
@@ -3,6 +3,7 @@ import 'package:basics/helpers/classes/maps/lister.dart';
 import 'package:basics/helpers/classes/strings/stringer.dart';
 import 'package:basics/helpers/classes/strings/text_check.dart';
 import 'package:basics/helpers/classes/strings/text_mod.dart';
+
 /// => TAMAM
 class Pathing {
   // -----------------------------------------------------------------------------
@@ -15,36 +16,32 @@ class Pathing {
 
   // --------------------
   /// TESTED : WORKS PERFECT
-  static String? getFirstPathNode({
-    required String? path
-  }){
-
-    if (path == null){
+  static String? getFirstPathNode({required String? path}) {
+    if (path == null) {
       return null;
-    }
-
-    else {
+    } else {
       /// FIRST PATH NODE IS CHAIN ROOT ID, in this example it's [phid_a] => 'phid_a/phid_b/phid_c'
       final String? _cleanedPath = TextMod.removeTextAfterLastSpecialCharacter(
         text: path,
         specialCharacter: '/',
       );
+
       /// => <String>[phid_a, phid_b, phid_c]
       final List<String>? _pathNodes = _cleanedPath?.split('/');
+
       /// => phid_c
       return Lister.checkCanLoop(_pathNodes) == true ? _pathNodes?.first : null;
     }
-
   }
+
   // --------------------
   /// TESTED : WORKS PERFECT
-  static String? getLastPathNode(String? path){
+  static String? getLastPathNode(String? path) {
     /// LAST PATH NODE IS the FURTHEST FROM ROOT ID, in this example it's [phid_c] => 'phid_a/phid_b/phid_c'
 
     String? _node;
 
-    if (TextCheck.isEmpty(path) == false){
-
+    if (TextCheck.isEmpty(path) == false) {
       final String? _fixed = Pathing.fixPathFormatting(path!);
       final String? _cleanedPath = TextMod.removeTextAfterLastSpecialCharacter(
         text: _fixed,
@@ -52,26 +49,22 @@ class Pathing {
       );
       final List<String>? _pathNodes = _cleanedPath?.split('/');
       _node = _pathNodes?.last;
-
     }
     return _node;
   }
+
   // --------------------
   /// TESTED : WORKS PERFECT
-  static List<String> getPathsLastNodes(List<String>? paths){
+  static List<String> getPathsLastNodes(List<String>? paths) {
     final List<String> _lastNodes = <String>[];
 
-    if (Lister.checkCanLoop(paths) == true){
-
-      for (final String path in paths!){
-
+    if (Lister.checkCanLoop(paths) == true) {
+      for (final String path in paths!) {
         final String? _node = getLastPathNode(path);
-        if (_node != null){
+        if (_node != null) {
           _lastNodes.add(_node);
         }
-
       }
-
     }
 
     return _lastNodes;
@@ -85,64 +78,57 @@ class Pathing {
   static String? getParentNode({
     required String path,
     required String node,
-  }){
-
-    if (TextCheck.isEmpty(path) == true || TextCheck.isEmpty(node) == true){
+  }) {
+    if (TextCheck.isEmpty(path) == true || TextCheck.isEmpty(node) == true) {
       return null;
-    }
-
-    else {
-
+    } else {
       final List<String> pathNodes = Pathing.splitPathNodes(path);
       final int _index = pathNodes.indexOf(node);
 
       /// non existent
-      if (_index == -1){
+      if (_index == -1) {
         return null;
       }
+
       /// first
-      else if (_index == 0){
+      else if (_index == 0) {
         return '';
       }
+
       /// found
       else {
         return pathNodes[_index - 1];
       }
-
     }
-
   }
+
   // --------------------
   /// AI TESTED
   static String? getSonNode({
     required String path,
     required String node,
-  }){
-
-    if (TextCheck.isEmpty(path) == true || TextCheck.isEmpty(node) == true){
+  }) {
+    if (TextCheck.isEmpty(path) == true || TextCheck.isEmpty(node) == true) {
       return null;
-    }
-
-    else {
-
+    } else {
       final List<String> pathNodes = Pathing.splitPathNodes(path);
       final int _index = pathNodes.indexOf(node);
 
       /// non existent
-      if (_index == -1){
+      if (_index == -1) {
         return null;
       }
+
       /// last
-      else if (_index == pathNodes.length - 1){
+      else if (_index == pathNodes.length - 1) {
         return '';
       }
+
       /// found
       else {
         return pathNodes[_index + 1];
       }
-
     }
-
   }
   // -----------------------------------------------------------------------------
 
@@ -153,54 +139,48 @@ class Pathing {
   static List<String> findPathsContainingSubstring({
     required List<String>? paths,
     required String? subString,
-  }){
+  }) {
     final List<String> _foundPaths = <String>[];
 
-    if (Lister.checkCanLoop(paths) && subString != null){
-
-      for (final String path in paths!){
-
+    if (Lister.checkCanLoop(paths) && subString != null) {
+      for (final String path in paths!) {
         final bool _containsSubString = TextCheck.stringContainsSubString(
           string: path,
           subString: subString,
         );
 
-        if (_containsSubString == true){
+        if (_containsSubString == true) {
           _foundPaths.add(path);
         }
-
       }
-
     }
 
     return _foundPaths;
   }
+
   // --------------------
   /// TESTED : WORKS PERFECT
   static List<String> findPathsContainingSubStrings({
     required List<String>? paths,
     required List<String>? subStrings,
-  }){
+  }) {
     List<String> _output = <String>[];
 
-    if (Lister.checkCanLoop(paths) == true && Lister.checkCanLoop(subStrings) == true){
-
-      for (final String phid in subStrings!){
-
+    if (Lister.checkCanLoop(paths) == true &&
+        Lister.checkCanLoop(subStrings) == true) {
+      for (final String phid in subStrings!) {
         final List<String> _foundPaths = findPathsContainingSubstring(
           paths: paths,
           subString: phid,
         );
 
-        if (Lister.checkCanLoop(_foundPaths) == true){
+        if (Lister.checkCanLoop(_foundPaths) == true) {
           _output = Stringer.addStringsToStringsIfDoNotContainThem(
             listToTake: _output,
             listToAdd: _foundPaths,
           );
         }
-
       }
-
     }
 
     return _output;
@@ -214,70 +194,59 @@ class Pathing {
   static List<String> addPathToPaths({
     required List<String>? paths,
     required String? path,
-  }){
-
+  }) {
     final List<String> _output = <String>[...?paths];
 
-    if (TextCheck.isEmpty(path) == false){
-
-      if (_output.contains(path) == false){
+    if (TextCheck.isEmpty(path) == false) {
+      if (_output.contains(path) == false) {
         _output.add(path!);
       }
-
     }
 
     return _output;
   }
+
   // --------------------
   /// TESTED : WORKS PERFECT
-  static List<String> splitPathNodes(String? path){
-
+  static List<String> splitPathNodes(String? path) {
     List<String> _divisions = <String>[];
 
-    if (TextCheck.isEmpty(path) == false){
-
+    if (TextCheck.isEmpty(path) == false) {
       // final String _cleaned = TextMod.removeTextAfterLastSpecialCharacter(path, '/');
       _divisions = path!.split('/').toList();
       _divisions.removeWhere((element) => element == '');
-
     }
 
     return _divisions;
   }
+
   // --------------------
   /// TESTED : WORKS PERFECT
-  static String combinePathNodes(List<String>? nodes){
+  static String combinePathNodes(List<String>? nodes) {
     String _path = '';
 
-    if (Lister.checkCanLoop(nodes) == true){
-
-      for (final String node in nodes!){
-
+    if (Lister.checkCanLoop(nodes) == true) {
+      for (final String node in nodes!) {
         _path = _path == '' ? '$node/' : '$_path$node/';
-
       }
-
     }
 
     return _path;
   }
+
   // --------------------
   /// TESTED : WORKS PERFECT
-  static String? removeLastPathNode({
-    required String? path
-  }){
+  static String? removeLastPathNode({required String? path}) {
     String? _output;
 
-    if (path != null){
-
+    if (path != null) {
       final String? _fixed = fixPathFormatting(path);
       final List<String> _nodes = splitPathNodes(_fixed);
 
-      if (Lister.checkCanLoop(_nodes) == true){
+      if (Lister.checkCanLoop(_nodes) == true) {
         _nodes.removeAt(_nodes.length - 1);
         _output = combinePathNodes(_nodes);
       }
-
     }
 
     return fixPathFormatting(_output);
@@ -288,8 +257,7 @@ class Pathing {
 
   // --------------------
   /// TESTED : WORKS PERFECT
-  static String? fixPathFormatting(String? path){
-
+  static String? fixPathFormatting(String? path) {
     /// NOTE : GOOD FORMAT SHOULD BE
     // 'chainK/blah_blah/phid/
     /// => no '/' in the beggining
@@ -297,10 +265,9 @@ class Pathing {
 
     String? _output = path?.trim();
 
-    if (TextCheck.isEmpty(_output) == false){
-
+    if (TextCheck.isEmpty(_output) == false) {
       /// REMOVE INITIAL SLASH IS EXISTS
-      if (_output![0] == '/'){
+      if (_output![0] == '/') {
         _output = TextMod.removeTextBeforeFirstSpecialCharacter(
           text: _output,
           specialCharacter: '/',
@@ -309,7 +276,7 @@ class Pathing {
 
       /// REMOVE LAST '//////' IF EXISTS
       int _lastIndex = _output!.length - 1;
-      if (_output[_lastIndex] == '/'){
+      if (_output[_lastIndex] == '/') {
         _output = TextMod.removeTextAfterLastSpecialCharacter(
           text: _output,
           specialCharacter: '/',
@@ -319,10 +286,9 @@ class Pathing {
 
       /// ASSURE LAST SLASH EXISTS
       _lastIndex = _output.length - 1;
-      if (_output[_lastIndex] != '/'){
+      if (_output[_lastIndex] != '/') {
         _output = '$_output/'; // should always keep one last slash
       }
-
     }
 
     return _output;
@@ -337,21 +303,34 @@ class Pathing {
     required String? parentNode,
     required List<String>? lastNodes,
     String previousPath = '', // ...xx/
-  }){
-
+  }) {
     final List<String> _paths = <String>[];
 
-    if (Lister.checkCanLoop(lastNodes) == true && parentNode != null){
-
-      for (final String phid in lastNodes!){
-
+    if (Lister.checkCanLoop(lastNodes) == true && parentNode != null) {
+      for (final String phid in lastNodes!) {
         _paths.add('$previousPath$parentNode/$phid/');
-
       }
-
     }
 
     return _paths;
+  }
+  // -----------------------------------------------------------------------------
+
+  /// CHECKERS
+
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  static bool checkIsPath(String? text) {
+    bool _output = false;
+
+    if (TextCheck.isEmpty(text) == false) {
+      _output = TextCheck.stringContainsSubString(
+        string: text,
+        subString: '/',
+      );
+    }
+
+    return _output;
   }
   // -----------------------------------------------------------------------------
 
@@ -365,9 +344,7 @@ class Pathing {
         strings: paths,
         invoker: 'blogPaths',
       );
-    }
-
-    else {
+    } else {
       blog('ALERT : paths are empty');
     }
   }
