be1ed5c826ab878ddc7abf400ad95015fcb9c7c5
RagehAz <rageh.az@gmail.com>
2024-06-11 21:31:42 +0200

    4.0.0 : popper fixed

--- lib/components/sensors/popper.dart
+++ lib/components/sensors/popper.dart
@@ -14,35 +14,58 @@ class Popper extends StatelessWidget {
   final Function? onBack;
   final Widget child;
   // --------------------------------------------------------------------------
-  static Future<void> onGoBack({
+  static Future<void> _onPopInvoked({
     required BuildContext context,
     required Function? onBack,
     required bool canGoBack,
+    required bool didPop,
   }) async {
 
-    if (onBack != null){
+    if (didPop == true){
+      // blog('==> DID POP ALREADY');
+    }
+
+    else if (onBack != null){
+      // blog('==> BACK IS OVERRIDEN : didPop : $didPop');
       await onBack.call();
     }
 
     else {
 
       if (canGoBack == true){
+        // blog('==> GOING BACK : didPop : $didPop');
         await Nav.goBack(
           context: context,
           invoker: 'Popper.onGoBack',
         );
       }
+      else {
+        // blog('==> BACK IS BLOCKED : didPop : $didPop');
+      }
 
     }
 
   }
-  // -------------------------
+  // --------------------
   @override
   Widget build(BuildContext context) {
-
+    // --------------------
+    /// I NEED
+    /// if can go back = can press the back buttons
+    /// back buttons : [ app bar back - android device back ]
+    ///
+    /// if onBack is defined = it overrides everything
+    /// so when tapping any of those buttons
+    /// it calls the overriding function
+    ///
+    /// that's it
+    // --------------------
+    /// DEPRECATED
     // return WillPopScope(
     //   key: const ValueKey<String>('Popper'),
     //   onWillPop: () async {
+    //
+    //     blog('on will pop aho');
     //
     //     await onGoBack(
     //       context: context,
@@ -54,17 +77,35 @@ class Popper extends StatelessWidget {
     //   },
     //   child: child,
     // );
-
+    /// WORKS GOOD
     return PopScope(
       key: const ValueKey<String>('Popper'),
-      canPop: canGoBack,
-      onPopInvoked: (bool value) => onGoBack(
+      canPop: false,
+      onPopInvoked: (bool value) => _onPopInvoked(
         context: context,
         canGoBack: canGoBack,
         onBack: onBack,
+        didPop: value,
       ),
       child: child,
     );
+
+    // return NavigatorPopHandler(
+    //   key: const ValueKey<String>('Popper'),
+    //   enabled: true,
+    //   onPop: () async {
+    //
+    //     blog('on pop aho');
+    //
+    //     await onGoBack(
+    //       context: context,
+    //       canGoBack: canGoBack,
+    //       onBack: onBack,
+    //     );
+    //
+    //   },
+    //   child: child,
+    // );
 
   }
   // --------------------------------------------------------------------------
