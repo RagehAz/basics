831fc8ee1ef41cdf1881c381f408b0f6ff3611c9
RagehAz <rageh.az@gmail.com>
2024-12-26 14:10:25 +0300

    5.8.0 : updated the waiter

--- lib/components/animators/widget_waiter.dart
+++ lib/components/animators/widget_waiter.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:basics/helpers/checks/tracers.dart';
 
 class WidgetWaiter extends StatelessWidget {
-  /// --------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
   const WidgetWaiter({
     required this.child,
     this.waitDuration = const Duration(milliseconds: 50),
     this.isOn = true,
     super.key
   });
-  /// --------------------------------------------------------------------------
+  // --------------------
   final Widget child;
   final Duration waitDuration;
   final bool isOn;
-  /// --------------------------------------------------------------------------
-  Future<bool> _rebuild() async {
-    await Future.delayed(waitDuration, () {});
-    return true;
-  }
   // --------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
@@ -26,25 +22,108 @@ class WidgetWaiter extends StatelessWidget {
     }
 
     else {
-      return FutureBuilder(
-        future: _rebuild(),
-        builder: (_, AsyncSnapshot<bool> snapshot){
-
-          final bool _build = snapshot.connectionState == ConnectionState.waiting ?
-          false : true;
-
-          if (_build == true){
-            return child;
-          }
-
-          else {
-            return const SizedBox.shrink();
-          }
-
-          },
+      return TheWaiter(
+        waitDuration: waitDuration,
+        child: child,
       );
     }
 
   }
-  /// --------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
+}
+
+class TheWaiter extends StatefulWidget {
+  // --------------------------------------------------------------------------
+  const TheWaiter({
+    required this.child,
+    required this.waitDuration,
+    super.key,
+  });
+  // --------------------
+  final Widget child;
+  final Duration waitDuration;
+  // --------------------
+  @override
+  _TheWaiterState createState() => _TheWaiterState();
+  // --------------------------------------------------------------------------
+}
+
+class _TheWaiterState extends State<TheWaiter> {
+  // --------------------------------------------------------------------------
+  bool _show = false;
+  // -----------------------------------------------------------------------------
+  @override
+  void initState() {
+    super.initState();
+  }
+  // --------------------
+  bool _isInit = true;
+  @override
+  void didChangeDependencies() {
+
+    if (_isInit && mounted) {
+      _isInit = false; // good
+
+      asyncInSync(() async {
+
+        await Future.delayed(widget.waitDuration);
+
+        if (mounted == true){
+          setState(() {
+            _show = true;
+          });
+        }
+
+      });
+
+    }
+    super.didChangeDependencies();
+  }
+  // --------------------
+  @override
+  void didUpdateWidget(TheWaiter oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.waitDuration.inMicroseconds != widget.waitDuration.inMicroseconds) {
+
+      asyncInSync(() async {
+
+        if (mounted == true){
+          setState(() {
+            _show = false;
+          });
+        }
+
+        await Future.delayed(widget.waitDuration);
+
+        if (mounted == true){
+          setState(() {
+            _show = true;
+          });
+        }
+
+      });
+
+    }
+  }
+  // --------------------
+  /*
+  @override
+  void dispose() {
+    super.dispose();
+  }
+   */
+  // -----------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+    // --------------------
+    if (_show == true){
+      return widget.child;
+    }
+    // --------------------
+    else {
+      return const SizedBox.shrink();
+    }
+    // --------------------
+  }
+  // -----------------------------------------------------------------------------
 }
