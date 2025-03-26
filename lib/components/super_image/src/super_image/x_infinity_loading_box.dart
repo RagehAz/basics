c771c49f5d3f5ac8119af69d707157a30f1d09ec
RagehAz <rageh.az@gmail.com>
2025-03-26 03:23:38 +0200

    6.6 : added engine loading trackers

--- lib/components/super_image/src/super_image/x_infinity_loading_box.dart
+++ lib/components/super_image/src/super_image/x_infinity_loading_box.dart
@@ -8,6 +8,7 @@ class InfiniteLoadingBox extends StatelessWidget {
     this.color,
     this.backgroundColor = Colorz.white50,
     this.milliseconds = 1900,
+    this.corners,
     super.key
   }); 
   // --------------------
@@ -16,6 +17,7 @@ class InfiniteLoadingBox extends StatelessWidget {
   final Color? color;
   final Color backgroundColor;
   final int milliseconds;
+  final dynamic corners;
   // --------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
@@ -25,70 +27,78 @@ class InfiniteLoadingBox extends StatelessWidget {
     final double _stripHeight = _height * 0.5;
     final Color _color = color ?? Colorz.white50;
 
+    final BorderRadius _borders = Borderers.superCorners(corners: corners);
+
     return Center(
-      child: Container(
-        width: _width,
-        height: _height,
-        color: backgroundColor,
-        child: WidgetFader(
-          fadeType: FadeType.repeatForwards,
-          // fadeType: FadeType.stillAtMin,
-          // min: 0.4,
-          curve: Curves.easeIn,
-          duration: Duration(milliseconds: milliseconds),
-          builder: (double value, Widget? child){
+      child: ClipRRect(
+        borderRadius: _borders,
+        child: Container(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: _borders,
+          ),
+          child: WidgetFader(
+            fadeType: FadeType.repeatForwards,
+            // fadeType: FadeType.stillAtMin,
+            // min: 0.4,
+            curve: Curves.easeIn,
+            duration: Duration(milliseconds: milliseconds),
+            builder: (double value, Widget? child){
 
-            return Stack(
-              children: <Widget>[
+              return Stack(
+                children: <Widget>[
 
-                /// UPPER HALF
-                Positioned(
-                  bottom: (_height * value * 1.6)  - _stripHeight - _stripHeight,
-                  left: 0,
-                  child: RotatedBox(
-                    quarterTurns: 2,
+                  /// UPPER HALF
+                  Positioned(
+                    bottom: (_height * value * 1.6)  - _stripHeight - _stripHeight,
+                    left: 0,
+                    child: RotatedBox(
+                      quarterTurns: 2,
+                      child: SizedBox(
+                        width: _width,
+                        height: _stripHeight,
+                        // color: Colorz.blue125,
+                        child: Transform.scale(
+                          scaleX: _width / _stripHeight,
+                          child: WebsafeSvg.asset(
+                            Iconz.boxShadowBlack,
+                            colorFilter: ColorFilter.mode(_color, BlendMode.srcIn),
+                            width: _width,
+                            height: _stripHeight,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  /// LOWER HALF
+                  Positioned(
+                    bottom: (_height * value * 1.6) + (_stripHeight * 0.999) - _stripHeight - _stripHeight,
+                    left: 0,
                     child: SizedBox(
                       width: _width,
                       height: _stripHeight,
-                      // color: Colorz.blue125,
                       child: Transform.scale(
                         scaleX: _width / _stripHeight,
                         child: WebsafeSvg.asset(
                           Iconz.boxShadowBlack,
+                          // fit: BoxFit.fitWidth,
                           colorFilter: ColorFilter.mode(_color, BlendMode.srcIn),
                           width: _width,
                           height: _stripHeight,
                         ),
                       ),
                     ),
-                  ),
-                ),
 
-                /// LOWER HALF
-                Positioned(
-                  bottom: (_height * value * 1.6) + (_stripHeight * 0.999) - _stripHeight - _stripHeight,
-                  left: 0,
-                  child: SizedBox(
-                    width: _width,
-                    height: _stripHeight,
-                    child: Transform.scale(
-                      scaleX: _width / _stripHeight,
-                      child: WebsafeSvg.asset(
-                        Iconz.boxShadowBlack,
-                        // fit: BoxFit.fitWidth,
-                        colorFilter: ColorFilter.mode(_color, BlendMode.srcIn),
-                        width: _width,
-                        height: _stripHeight,
-                      ),
-                    ),
                   ),
 
-                ),
+                ],
+              );
 
-              ],
-            );
-
-          },
+            },
+          ),
         ),
       ),
     );
