7be774ca8b319c2f720e14d928dcb95b99c68e7e
RagehAz <rageh.az@gmail.com>
2025-03-29 02:47:19 +0200

    6.6 : COLORZ SEPARATED

--- lib/bldrs_theme/classes/colorz.dart
+++ lib/bldrs_theme/classes/colorz.dart
@@ -8,6 +8,7 @@ abstract class Colorz {
   static const Color black0 = Color.fromARGB(0, 0, 0, 0);
   static const Color black10 = Color.fromARGB(10, 0, 0, 0);
   static const Color black20 = Color.fromARGB(20, 0, 0, 0 );
+  static const Color black30 = Color.fromARGB(30, 0, 0, 0 );
   static const Color black50 = Color.fromARGB(50, 0, 0, 0);
   static const Color black80 = Color.fromARGB(80, 0, 0, 0);
   static const Color black125 = Color.fromARGB(125, 0, 0, 0);
@@ -85,17 +86,30 @@ abstract class Colorz {
   static const Color lightGrey255 = Color.fromARGB(255, 220, 220, 220);
   // --------------------
   static const Color darkGrey255 = Color.fromARGB(255, 180, 180, 180);
+  static const Color darkGrey80 = Color.fromARGB(80, 180, 180, 180);
   // --------------------
   static const Color facebook = Color.fromARGB(255, 59, 89, 152);
   static const Color linkedIn = Color.fromARGB(255, 0, 115, 176);
   static const Color googleRed = Color.fromARGB(255, 234, 67, 53);
+  static const Color youtube = Color.fromRGBO(255, 0, 0, 1);
+  static const Color instagram = Color.fromRGBO(193, 53, 132, 1);
+  static const Color pinterest = Color.fromRGBO(230, 0, 35, 1);
   static const Color purple = Color.fromARGB(255, 87, 42, 105);
+  static const Color tiktok = Color.fromRGBO(0, 0, 0, 1);
+  static const Color twitter = Color.fromRGBO(38, 167, 222, 1);
+  static const Color snapChat = Color.fromRGBO(255, 252, 0, 1);
+  static const Color behance = Color.fromRGBO(5, 62, 255, 1);
+  static const Color vimeo = Color.fromRGBO(134, 201, 239, 1);
+  static const Color googleMaps = Color.fromRGBO(52, 168, 83, 1);
+  static const Color appStore = Color.fromRGBO(0, 122, 255, 1);
+  static const Color googlePlay = Color.fromRGBO(255, 212, 0, 1);
+  static const Color appGallery = Color.fromRGBO(206, 14, 45, 1);
   // --------------------
   static const Color telegramLightBlue = Color.fromARGB(255, 56, 176, 227);
   static const Color telegramDarkBlue = Color.fromARGB(255, 29, 147, 210);
   // --------------------
   static const Color appBarColor = white20;
-  static const Color bzPageBGColor = Colorz.black80;
+  static const Color bzPageBGColor = black80;
   // -------------------------------------------------------------------------
   static const List<Color> allColorz = <Color>[
     bloodTest,
@@ -153,10 +167,10 @@ abstract class Colorz {
     white80,
     white125,
     white200,
-    white230 ,
-    white255 ,
-    grey50 ,
-    grey80 ,
+    white230,
+    white255,
+    grey50,
+    grey80,
     grey255,
     lightGrey255,
     darkGrey255,
