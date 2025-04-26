3d9022fcb7380ac755d809414cf5d5878f3de552
RagehAz <rageh.az@gmail.com>
2025-04-27 00:19:05 +0300

    6.9 : YA RAB

--- lib/mediator/video_maker/video_processor.dart
+++ lib/mediator/video_maker/video_processor.dart
@@ -1,56 +1,55 @@
-import 'dart:io';
-import 'package:basics/helpers/strings/text_check.dart';
-import 'package:flutter_native_video_trimmer/flutter_native_video_trimmer.dart';
+// import 'dart:io';
+// import 'package:basics/helpers/strings/text_check.dart';
+// // import 'package:flutter_native_video_trimmer/flutter_native_video_trimmer.dart';
+// import '../../helpers/checks/error_helpers.dart';
+// import '../../helpers/checks/tracers.dart';
 
-import '../../helpers/checks/error_helpers.dart';
-import '../../helpers/checks/tracers.dart';
-
-abstract class VideoProcessor {
-  // -----------------------------------------------------------------------------
-
-  /// TRIM
-
-  // --------------------
-  ///
-  static Future<File?> trimVideo({
-    required File? file,
-    required int? startMs,
-    required int? endMs,
-    required bool mute,
-  }) async {
-    File? _output;
-
-    if (file != null && startMs != null && endMs != null){
-
-      await tryAndCatch(
-        invoker: 'trimVideo',
-        functions: () async {
-
-          final VideoTrimmer videoTrimmer = VideoTrimmer();
-
-          await videoTrimmer.loadVideo(file.path);
-
-          final String? trimmedPath = await videoTrimmer.trimVideo(
-            startTimeMs: startMs,     // Start time in milliseconds
-            endTimeMs: endMs,    // End time in milliseconds (5 seconds)
-            includeAudio: !mute, // Optional, default is true
-          );
-
-          if (TextCheck.isEmpty(trimmedPath) == false){
-            _output = File(trimmedPath!);
-          }
-
-          // await videoTrimmer.clearCache();
-
-        },
-        onError: (String? error){
-          blog('trimVideo : error : $error');
-        },
-      );
-
-    }
-
-    return _output;
-  }
-  // -----------------------------------------------------------------------------
-}
+// abstract class VideoProcessor {
+//   // -----------------------------------------------------------------------------
+//
+//   /// TRIM
+//
+//   // --------------------
+//   ///
+//   static Future<File?> trimVideo({
+//     required File? file,
+//     required int? startMs,
+//     required int? endMs,
+//     required bool mute,
+//   }) async {
+//     File? _output;
+//
+//     if (file != null && startMs != null && endMs != null){
+//
+//       await tryAndCatch(
+//         invoker: 'trimVideo',
+//         functions: () async {
+//
+//           final VideoTrimmer videoTrimmer = VideoTrimmer();
+//
+//           await videoTrimmer.loadVideo(file.path);
+//
+//           final String? trimmedPath = await videoTrimmer.trimVideo(
+//             startTimeMs: startMs,     // Start time in milliseconds
+//             endTimeMs: endMs,    // End time in milliseconds (5 seconds)
+//             includeAudio: !mute, // Optional, default is true
+//           );
+//
+//           if (TextCheck.isEmpty(trimmedPath) == false){
+//             _output = File(trimmedPath!);
+//           }
+//
+//           // await videoTrimmer.clearCache();
+//
+//         },
+//         onError: (String? error){
+//           blog('trimVideo : error : $error');
+//         },
+//       );
+//
+//     }
+//
+//     return _output;
+//   }
+//   // -----------------------------------------------------------------------------
+// }
